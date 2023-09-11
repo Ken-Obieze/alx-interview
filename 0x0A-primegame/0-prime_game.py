d832d5716e439b5d41d9fe061aa19edf2a026ccc
Ken-Obieze <kenneth.obieze@outlook.com>
2023-09-11 04:35:33 +0100

    final fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
-"""Prime Game Module"""
-
-
-def is_prime(n):
-    """Check if a number is prime"""
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+"""Module define isWinner fuction for Prime Game."""
 
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game"""
+    """
+    Determine the winner of the prime game.
+    Args:
+        x (int): The number of rounds to be played.
+        nums (list of int): An array of n values for each round.
+    Returns:
+        str or None: The name of the player who won the most rounds.
+    """
+
+
+    def is_prime(n):
+        """
+        Check if a number is prime.
+        Args:
+            n (int): The number to check.
+        Returns:
+            bool: True if the number is prime, False otherwise.
+        """
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
     if not nums or x < 1:
         return None
 
     wins = {'Maria': 0, 'Ben': 0}
 
     for n in nums:
-        primes = sum([is_prime(i) for i in range(n + 1)])
+        primes = sum(1 for i in range(2, n + 1) if is_prime(i))
         if primes % 2 == 0:
             wins['Ben'] += 1
         else:
