e3bd2f8dadad0f657c2ef9a19eb6197703cb2da8
Ken-Obieze <kenneth.obieze@outlook.com>
2023-10-26 19:54:25 +0100

    UTF8 validator fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,28 +5,29 @@
 def validUTF8(data):
     """
     Check if a given data set represents a valid UTF-8 encoding.
-
     Args:
         data (list): A list of integers representing bytes of data.
-
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    consecutive_ones = 0
+    remaining_bytes = 0
 
     for byte in data:
-        if byte < 256:
-            if consecutive_ones == 0:
-                if (byte >> 7) & 1 == 0:
-                    continue
-                elif (byte >> 6) & 3 == 2:
-                    return False
-                consecutive_ones = 1
-            else:
-                if (byte >> 6) & 3 == 2:
-                    consecutive_ones -= 1
-                else:
-                    return False
+        byte = byte & 0xFF
+
+        if remaining_bytes == 0:
+            if byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            elif byte >> 7:
+                return False
         else:
-            return False
-    return consecutive_ones == 0
+            if byte >> 6 != 0b10:
+                return False
+
+            remaining_bytes -= 1
+
+    return remaining_bytes == 0
